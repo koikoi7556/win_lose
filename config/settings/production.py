ddf9f64b3d6576d7f0c78cd5a445439916440f28
koikoi7556 <koikoi7556@gmail.com>
2019-10-28 22:15:56 +0900

    settingsのloggerをコメント
    herokuでFileNotFoundErrorがでたので対処

--- config/settings/production.py
+++ config/settings/production.py
@@ -35,45 +35,49 @@ DATABASES = {
 # ビューから例外が漏れてしまった場合にビュー何でのデータベースの保存更新削除がすべてロールバックされる。
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 # Logging
-LOGGING = {
-    # バージョンは「1」固定
-    'version': 1,
-    # 既存のログ設定を無効化しない
-    'disable_existing_loggers': False,
-    # ログフォーマット
-    'formatters': {
-        # 本番用
-        'production': {
-            'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d '
-                      '%(pathname)s:%(lineno)d %(message)s'
-        },
-    },
-    # ハンドラ
-    'handlers': {
-        # ファイル出力用ハンドラ
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/{}/app.log'.format(PROJECT_NAME),
-            'formatter': 'production',
-        },
-    },
-    # ロガー
-    'loggers': {
-        # 自作アプリケーション全般のログを拾うロガー
-        '': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        # Django本体が出すログ全般を拾うロガー
-        'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    },
-}
+# Heroku デプロイの"python manage.py migrate"でエラーが起きるためコメント
+"""以下実際のエラー文
+FileNotFoundError: [Errno 2] No such file or directory: '/var/log/app/app.log'
+"""
+# LOGGING = {
+#     # バージョンは「1」固定
+#     'version': 1,
+#     # 既存のログ設定を無効化しない
+#     'disable_existing_loggers': False,
+#     # ログフォーマット
+#     'formatters': {
+#         # 本番用
+#         'production': {
+#             'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d '
+#                       '%(pathname)s:%(lineno)d %(message)s'
+#         },
+#     },
+#     # ハンドラ
+#     'handlers': {
+#         # ファイル出力用ハンドラ
+#         'file': {
+#             'level': 'INFO',
+#             'class': 'logging.FileHandler',
+#             'filename': '/var/log/{}/app.log'.format(PROJECT_NAME),
+#             'formatter': 'production',
+#         },
+#     },
+#     # ロガー
+#     'loggers': {
+#         # 自作アプリケーション全般のログを拾うロガー
+#         '': {
+#             'handlers': ['file'],
+#             'level': 'INFO',
+#             'propagate': False,
+#         },
+#         # Django本体が出すログ全般を拾うロガー
+#         'django': {
+#             'handlers': ['file'],
+#             'level': 'INFO',
+#             'propagate': False,
+#         },
+#     },
+# }
 
 
 # データベースの接続情報を環境変数から指定できるようにする（変数名は「DATABASE_URL」）。無料のHerokuデータベースを特に設定せずに使えるのはこの仕組みを利用しているから。
