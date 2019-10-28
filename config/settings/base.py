ef458e47fb26d926617553efe4fa040ba29a55dc
koikoi7556 <koikoi7556@gmail.com>
2019-10-28 23:10:13 +0900

    static_root修正

--- config/settings/base.py
+++ config/settings/base.py
@@ -127,11 +127,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # 静的ファイルの配信元。collestatic コマンドでのコピー先。DEBUG=falseのとき必要。
 # STATICFILES_DIRとは別ディレクトリをしてする必要がある。
-STATIC_ROOT = '/var/www/{}/static'.format(PROJECT_NAME)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfolder')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/{}/media'.format(PROJECT_NAME)
+STATIC_ROOT = os.path.join(BASE_DIR, 'mediafolder')
 
 # 認証システム関係のリダイレクト先
 LOGIN_REDIRECT_URL = 'janken'
